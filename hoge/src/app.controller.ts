098f9b4e6bd85ae3d772d9029d8e9210ececa39b
sabo10o29 <alex29sabo10@gmail.com>
2023-09-30 15:10:35 +0000

    Add prisma sample

--- hoge/src/app.controller.ts
+++ hoge/src/app.controller.ts
@@ -1,17 +1,30 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { PrismaClient, books } from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
+    // .env sample
     const configService = new ConfigService();
     const databaseUrl = configService.get<string>('DATABASE_URL');
     const host = configService.get<string>('DB_HOST');
-    console.log('@@@@@@', databaseUrl, host);
+    console.log('@', databaseUrl, host);
+    // prisma client sample
+    const prisma = new PrismaClient();
+    const book: books = await prisma.books.create({
+      data: {
+        name: 'test name',
+        title: 'test title',
+      },
+    });
+    const allBooks = await prisma.books.findMany();
+    const outputBook = allBooks[0];
+    console.log('@', book, allBooks, outputBook);
     return this.appService.getHello();
   }
 }
