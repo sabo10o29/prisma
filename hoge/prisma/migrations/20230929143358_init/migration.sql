f02f596d0f0a72c06e17217931b0647f72c3ffe7
sabo10o29 <alex29sabo10@gmail.com>
2023-09-30 14:33:05 +0000

    update

--- hoge/prisma/migrations/20230929143358_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `name` to the `books` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `books` ADD COLUMN `name` VARCHAR(191) NOT NULL;
